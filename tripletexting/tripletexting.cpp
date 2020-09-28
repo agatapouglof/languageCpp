d7355cce42b34afc2ea9f771074b64681463cb0f
agatapouglof <001ndj@gmail.com>
2020-09-28 21:36:06 +0200

    tripletexting

---
+++ tripletexting/tripletexting.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+int main(){
+    string input ;
+    cin >> input ;
+    int le = input.length() / 3;
+
+    rep(i, le){
+        if(input[i] == input[i+le]){
+            cout << input[i];
+        }else{
+            if(input[i] == input[i+2*le]) cout << input[i];
+            else cout << input[i+le];
+        }
+    }
+    return 0;
+}
