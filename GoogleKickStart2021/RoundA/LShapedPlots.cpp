047da7055775e6d55f6ea5c7d447b4bc74f4767c
agatapouglof <001ndj@gmail.com>
2021-11-08 21:41:37 +0100

    lyklagangriti - Keystrokes

--- GoogleKickStart2021/RoundA/LShapedPlots.cpp
+++ GoogleKickStart2021/RoundA/LShapedPlots.cpp
@@ -70,20 +70,6 @@ int main(){
     }
     
     
-    // for (int i = 0; i < rows; i++)
-    // {   
-    //     for (int j = 0; j < cols; j++)
-    //     {
-    //         cout << matrix[i][j] << " ";
-    //     }
-    //     cout << "\n";
-        
-    // }
-
-
-
-
-    
 
 
     return 0;
